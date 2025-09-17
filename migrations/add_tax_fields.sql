a3915ee0ac22e9aa77a7612005489c11cdfd6a63
mohansky <mohansky@gmail.com>
2025-09-17 07:15:59 +0530

    Remove unnecessary files and update configuration

--- migrations/add_tax_fields.sql
+++
@@ -1,8 +0,0 @@
--- Add GST percentage and tax inclusion fields to products table
-ALTER TABLE products ADD COLUMN gstPercentage REAL DEFAULT 5;
-ALTER TABLE products ADD COLUMN taxInclusive INTEGER DEFAULT 0;
-
--- Update existing products to have default tax settings
-UPDATE products
-SET gstPercentage = 5, taxInclusive = 0
-WHERE gstPercentage IS NULL OR taxInclusive IS NULL;
